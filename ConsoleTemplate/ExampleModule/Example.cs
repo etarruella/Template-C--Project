d06caf56b44f671a8c2c1b8d1bd2e40813df09f2
Vera-Profe <Vera.Masc98@gmail.com>
2025-05-07 13:16:20 +0200

    Settings and examples

---
+++ ConsoleTemplate/ExampleModule/Example.cs
@@ -0,0 +1,24 @@
+
+//Nombre del módulo
+namespace ExampleMod{
+
+    /// <summary>
+    /// Sirve como nexo del módulo
+    /// </summary>
+    public class Module{
+
+        /// <summary>
+        /// Punto de entrada para ejecutar el módulo
+        /// </summary>
+        public static void Execute(){
+            
+            /*
+                funcionalidades del módulo
+            */
+
+        }
+
+    }
+    
+
+}
--- ConsoleTemplate/Program.cs
+++ ConsoleTemplate/Program.cs
@@ -1,13 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-/// <summary>
-/// a
-/// </summary>
 internal class Program
 {
-    static void Main(string[] args)
-    {
+    static void Main(string[] args){
         Console.WriteLine("Hello World!");
+        //Cómo llamar a un módulo concreto
+        ExampleMod.Module.Execute();
     }
 }
