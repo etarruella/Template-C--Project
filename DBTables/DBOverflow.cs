dbeaa71a14a48d3cbe3825ad9e62dd1ef5ab158e
Erik Tarruella <eriktarruella@gmail.com>
2025-05-08 10:27:04 +0000

    feat: adding comment class

---
+++ DBTables/DBOverflow.cs
@@ -0,0 +1,33 @@
+namespace DBTables.Overflow
+
+{
+
+    public class Comment {
+        
+        public int id;
+        public Datetime creationDate;
+
+        public int postId;
+
+        public int? score;
+
+        public string text;
+
+        public int userId;
+
+        public Comment(int id, DateTime creationDate, int postId, int score, string text, int userId) {
+            this.id = id;
+            this.creationDate = creationDate;
+            this.postId = postId;
+            this.score = score;
+            this.text = text;
+            this.userId = userId;
+        }
+
+        public Comment(string text) {
+            this.text = text;
+        }
+
+    }
+
+}
